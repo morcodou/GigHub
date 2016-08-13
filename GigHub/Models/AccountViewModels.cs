1eb0ed2e67ac1f31c3ae072830b1920b47fbb017
Lenovo <mcodou@gmail.com>
2016-08-13 19:47:25 -0400

    move LoginViewModel

--- GigHub/Models/AccountViewModels.cs
+++ GigHub/Models/AccountViewModels.cs
@@ -46,21 +46,21 @@ namespace GigHub.Models
         public string Email { get; set; }
     }
 
-    public class LoginViewModel
-    {
-        [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+    ////public class LoginViewModel
+    ////{
+    ////    [Required]
+    ////    [Display(Name = "Email")]
+    ////    [EmailAddress]
+    ////    public string Email { get; set; }
 
-        [Required]
-        [DataType(DataType.Password)]
-        [Display(Name = "Password")]
-        public string Password { get; set; }
+    ////    [Required]
+    ////    [DataType(DataType.Password)]
+    ////    [Display(Name = "Password")]
+    ////    public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
-        public bool RememberMe { get; set; }
-    }
+    ////    [Display(Name = "Remember me?")]
+    ////    public bool RememberMe { get; set; }
+    ////}
 
 
 
