d96f9edcf753d978a4da38b70ce80f66727a4e24
Lenovo <mcodou@gmail.com>
2016-08-14 00:34:23 -0400

    Adding Following

---
+++ GigHub/Controllers/FollowingsController.cs
@@ -0,0 +1,43 @@
+﻿using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+
+namespace GigHub.Controllers
+{
+
+    [Authorize]
+    public class FollowingsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public FollowingsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Follow(FollowingDto dto)
+        {
+            var userid = User.Identity.GetUserId();
+
+            if (_context.Followings.Any(f => f.FollowerId == userid && f.FolloweeId == dto.FolloweeId))
+            {
+                return BadRequest("Following already exists.");
+            }
+
+            var following = new Following()
+            {
+                FollowerId = userid,
+                FolloweeId = dto.FolloweeId
+            };
+
+            _context.Followings.Add(following);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ GigHub/Dtos/FollowingDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GigHub.Dtos
+{
+    public class FollowingDto
+
+    {
+        public string FolloweeId { get; set; }
+
+    }
+}
